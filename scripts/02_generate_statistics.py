56a18de45b2e68a4384eac9a8e29014983b4162a
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-20 23:32:49 -0800

    02_generate_statistics script outline

---
+++ scripts/02_generate_statistics.py
@@ -0,0 +1,5 @@
+# Load match history
+# Compute win rates per strategy
+# Generate a confusion matrix (matchup matrix)
+# Generate a heatmap of most common choices
+# Save statistics as CSV and plots
