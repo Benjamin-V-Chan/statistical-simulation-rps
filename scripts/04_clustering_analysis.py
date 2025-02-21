30776b495ae29acf62ca85c20bfe5c8efbe70f72
BenjaminChan <benjamin.v.chan@gmail.com>
2025-02-20 23:44:15 -0800

    04_clustering_analysis script complete

--- scripts/04_clustering_analysis.py
+++ scripts/04_clustering_analysis.py
@@ -1,4 +1,13 @@
-# Load match data
-# Convert moves to numerical format
-# Apply K-Means or DBSCAN clustering
-# Visualize clusters
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("data/match_history.csv")
+df["Move_Num"] = df["P1_Move"].map({"Rock": 0, "Paper": 1, "Scissors": 2})
+X = df[["Move_Num"]].values
+
+kmeans = KMeans(n_clusters=3)
+df["Cluster"] = kmeans.fit_predict(X)
+
+plt.scatter(df.index, df["Move_Num"], c=df["Cluster"])
+plt.title("Clustering of Player Behavior")
+plt.savefig("outputs/clustering_results.png")
